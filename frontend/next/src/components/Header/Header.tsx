9e8cd20865f06fc83307d2e656531fe4326ae559
Sylek <sylwia.werner@oke.pl>
2025-01-24 00:17:05 +0100

    add logout

--- frontend/next/src/components/Header/Header.tsx
+++ frontend/next/src/components/Header/Header.tsx
@@ -1,10 +1,30 @@
+'use client';
+
 import { FaFacebook } from 'react-icons/fa';
 import $ from './Header.module.scss';
+import clsx from 'clsx';
+import { useAuthStore } from '@/stores/useAuthStore';
+import { useRouter } from 'next/navigation';
 
 export const Header = () => {
+    const { logout } = useAuthStore();
+    const router = useRouter();
+
+    const handleLogout = () => {
+        logout();
+        router.push('/login');
+    };
+
     return (
         <header className={$.header}>
             <FaFacebook />
+            <button
+                type="submit"
+                className={clsx($.logoutButton, 'btn')}
+                onClick={handleLogout}
+            >
+                Logout
+            </button>
         </header>
     );
 };
