a8ab323176643c90a496c33335f839d3bd72fc53
Sylek <sylwia.werner@oke.pl>
2025-01-25 18:42:40 +0100

    add nuxt with ts, eslint and prettier

--- frontend/nuxt/nuxt.config.ts
+++ frontend/nuxt/nuxt.config.ts
@@ -1,4 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true, // Server-Side Rendering
-  target: 'server', // Server deployment
+    srcDir: 'src',
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    modules: ['@nuxt/eslint'],
+    devServer: {
+        port: 3001,
+    },
 });
